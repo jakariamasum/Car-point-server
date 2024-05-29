5cd6e4620e8105e53bee5a64ec1455f0abb048b1
jakariamasum <jmphy698@gmail.com>
2024-05-29 22:05:08 +0600

    jwt token generate and verify

--- index.js
+++ index.js
@@ -24,6 +24,46 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const userListCollection = client
+      .db("carCollection")
+      .collection("usersList");
+    const productListingsBySellers = client
+      .db("carCollection")
+      .collection("oldCarsByUsers");
+    const savedAdsListCollection = client
+      .db("carCollection")
+      .collection("savedAdsList");
+    const feedbackListCollection = client
+      .db("carCollection")
+      .collection("allFeedbacks");
+    const allBidsCollection = client.db("carCollection").collection("allBids");
+
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_WEB_TOKEN, {
+        expiresIn: "72h",
+      });
+      res.send({ token });
+    });
+
+    // verify token middleware
+    const verifyToken = (req, res, next) => {
+      const tokenAuthorization = req.headers.authorization;
+      if (!tokenAuthorization) {
+        return res.status(401).send({ message: "Unauthorized" });
+      }
+      const token = tokenAuthorization.split(" ")[1];
+      // verify token
+      jwt.verify(token, process.env.ACCESS_WEB_TOKEN, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "Unauthorized" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
